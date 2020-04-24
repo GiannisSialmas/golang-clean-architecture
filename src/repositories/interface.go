c6cc7ba195f9a909c0c68978327bf9fe5b6f0e8e
Giannis Sialmas <giannissl@projectagora.com>
2020-04-25 00:08:38 +0300

    Change filenames for better understanding

--- src/repositories/interface.go
+++ src/repositories/interface.go
@@ -9,6 +9,7 @@ type IUserRepository interface {
 	Create(user models.User) (models.User, error)
 	GetOne(id uint) (models.User, error)
 	GetAll() ([]models.User, error)
-	Update(user models.User) (models.User, error)
+	GetByEmail(email string) (models.User, error)
+	// Update(user models.User) (models.User, error)
 	Delete(id uint) error
 }
